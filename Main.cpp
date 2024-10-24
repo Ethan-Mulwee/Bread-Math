043d5d41f81403e8de3544dc6b5d5cd444d77e08
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2024-10-23 22:48:47 -0400

    Matrix multiplication

--- Main.cpp
+++ Main.cpp
@@ -4,14 +4,40 @@
 using namespace bMath;
 
 int main() {
-    Vector3 vec(4,5,6);
-    vec.normalize();
-    std::cout << vec.x << ", " <<  vec.y << ", " << vec.z << std::endl;
+    // Vector3 vec(4,5,6);
+    // vec.normalize();
+    // std::cout << vec.x << ", " <<  vec.y << ", " << vec.z << std::endl;
 
-    Matrix<2,2> mat = Matrix<2,2>();
-    mat.data[1][1] = 2;
-    Matrix<2,2> mat2 = Matrix<2,2>();
-    mat2.data[1][1] = 3;
-    Matrix mat3 = mat + mat2;
-    std::cout << mat3.data[1][1] << std::endl;
+    // Matrix<2,2> mat = Matrix<2,2>();
+    // mat.data[1][1] = 2;
+    // Matrix<2,2> mat2 = Matrix<2,2>();
+    // mat2.data[1][1] = 3;
+    // Matrix mat3 = mat + mat2;
+    // std::cout << mat3.data[1][1] << std::endl;
+
+    // Matrix mat4 = mat * mat2;
+    // std::cout << mat4.data[1][1] << std::endl;
+
+    Matrix mat5 = Matrix<2,3>();
+    mat5.data[0][0] = 4;
+    mat5.data[0][1] = 4;
+    mat5.data[0][2] = 4;
+    mat5.data[1][0] = 4;
+    mat5.data[1][1] = 4;
+    mat5.data[1][2] = 4;
+    // mat5.log();
+    Matrix mat6 = Matrix<3,2>();
+    mat6.data[0][0] = 3;
+    mat6.data[0][1] = 3;
+    mat6.data[1][0] = 3;
+    mat6.data[1][1] = 3;
+    mat6.data[2][0] = 3;
+    mat6.data[2][1] = 3;
+
+
+    Matrix mat7 = mat6*mat5;
+    mat7.log();
+
+    
+
 }
--- include/bMath/Matrix.hpp
+++ include/bMath/Matrix.hpp
@@ -2,17 +2,17 @@
 #define BMATH_MATRIX
 
 #include <assert.h>
+#include <iostream>
 
 namespace bMath {
     template <int rows, int cols>
     struct Matrix {
         float data[rows][cols] = {};
-        const int r = rows; const int c = cols;
+        const static int r = rows; const static int c = cols;
 
         Matrix() {}
 
         Matrix operator+(const Matrix &m) const {
-            assert(rows == m.r && c == m.c);
             Matrix<rows,cols> newMat;
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < cols; j++) {
@@ -20,6 +20,29 @@ namespace bMath {
                 }
             }
             return newMat;
+        }
+        template <typename T>
+        auto operator*(const T &m) const {
+            assert(cols == m.r);
+            Matrix<rows,m.c> newMat;
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < m.c; j++) {
+                    float total = 0;
+                    for (int k = 0; k < cols; k++) {
+                        total += data[i][k]*m.data[k][i];
+                    }
+                    newMat.data[i][j] = total;
+                }
+            }
+            return newMat;
+        }
+
+        void log() const {
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    std::cout << data[i][j] << std::endl;
+                }
+            }
         }
     };
 }
