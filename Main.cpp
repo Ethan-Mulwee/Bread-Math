60223fd1d132ecd7b48f7ddf6c4d2ad20d88fee2
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2024-11-01 11:08:53 -0400

    fixed 1 letter matrix mult works

--- Main.cpp
+++ Main.cpp
@@ -5,16 +5,16 @@
 using namespace bMath;
 
 int main() {
-  Matrix<float,3,3> m1(
-    5,3,2,
-    0,2,1,
-    9,2,4
+  Matrix<float, 3, 3> m1(
+      5, 6, 2,
+      0, 2, 1,
+      9, 2, 4
   );
-  Matrix<float,3,3> m2(
-    10,3,2,
-    0,4,1,
-    9,1,2
+  Matrix<float, 3, 3> m2(
+      10, 4, 2,
+      0, 4, 1,
+      9, 1, 2
   );
 
-  std::cout << m1*m2;
+  std::cout << m1 * m2 << "\n";
 }
