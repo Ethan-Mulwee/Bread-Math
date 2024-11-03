39769eb4839a74bf46294926b9f48b8070d0ceef
Ethan <167151595+Ethan-Mulwee@users.noreply.github.com>
2024-11-03 12:05:30 +0000

    added raylib for debugging

--- Main.cpp
+++ Main.cpp
@@ -1,15 +1,39 @@
 #include <iostream>
 #include "bMath/bMath.hpp"
+#include <raylib.h>
 
-using namespace bMath;
+Camera camera;
+const int axisLength = 4;
 
 int main() {
-    Triangle triangle(Vector3(1,0,-1), Vector3(1,2,2), Vector3(4,0,0));
-    Ray ray(Vector3(-1,2,-2), Vector3(1,-0.5,1));
 
-    RayIntersection intersection = Raycast(ray, triangle);
+    camera.position = Vector3{5,5,5};
+    camera.target = Vector3{0,0,0};
+    camera.up = Vector3{0,1,0};
+    camera.fovy = 45;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    bMath::Triangle triangle(bMath::Vector3(1,0,-1), bMath::Vector3(1,2,2), bMath::Vector3(4,0,0));
+    bMath::Ray ray(bMath::Vector3(-1,2,-2), bMath::Vector3(1,-0.5,1));
+
+    bMath::RayIntersection intersection = Raycast(ray, triangle);
     std::cout << intersection.point << std::endl;
 
-    Matrix<3,3> m;
+    bMath::Matrix<3,3> m;
     std::cout << m << std::endl;
+
+    InitWindow(500,500,"test");
+
+    while(!WindowShouldClose()) {
+        UpdateCamera(&camera, CAMERA_PERSPECTIVE);
+        BeginDrawing();
+            ClearBackground(Color{35,35,35,255});
+            BeginMode3D(camera);
+                DrawGrid(5,1);
+                DrawLine3D(Vector3{-axisLength,0,0}, Vector3{axisLength,0,0}, RED);
+                DrawLine3D(Vector3{0,-axisLength,0}, Vector3{0,axisLength+0.5,0}, GREEN);
+                DrawLine3D(Vector3{0,0,-axisLength}, Vector3{0,0,axisLength}, BLUE);
+            EndMode3D();
+        EndDrawing();
+    }
 }
