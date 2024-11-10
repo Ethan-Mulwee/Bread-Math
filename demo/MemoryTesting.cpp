46e3f4a122802fc5ed388eba3065520ea53cfb66
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2024-11-10 11:58:07 -0500

    memory testing

---
+++ demo/MemoryTesting.cpp
@@ -0,0 +1,10 @@
+#include "bMath/bMath.hpp"
+#include "bMath/iostream.hpp"
+
+// g++ MemoryTesting.cpp -I../include -g
+
+using namespace bMath;
+
+int main() {
+  int test[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+}
