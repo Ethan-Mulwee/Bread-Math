a21bf8192fde9a763e3f3522584d0e7eb621cff4
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2024-11-10 16:38:33 -0500

    basic raylib conversion functions

---
+++ demo/RaylibTesting.cpp
@@ -0,0 +1,10 @@
+#include <raylib.h>
+#include "bMath/bMath.hpp"
+#include "bMath/ext/raylib.hpp"
+#include "bMath/ext/iostream.hpp"
+
+// g++ RaylibTesting.cpp -lraylib -I../include
+
+int main() {
+  
+}
