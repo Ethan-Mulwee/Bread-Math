4097eb98a03227fa3f1a4b6ffc95f1cfec9c54ac
Ethan <167151595+Ethan-Mulwee@users.noreply.github.com>
2024-11-12 16:51:40 +0000

    matrix functions

--- demo/MatrixTesting.cpp
+++ demo/MatrixTesting.cpp
@@ -9,23 +9,44 @@ using namespace bMath;
 
 int main() {
   // Performance testing
-  Matrix<float, 3, 3> m1(
-    5,4,3,
-    5,4,3,
-    5,4,3
+  Matrix3 m1(
+    2,4,3,
+    5,2,3,
+    5,4,2
   );
 
-  std::cout << m1 << "\n";
+  Matrix2 m2(
+    1,2,
+    3,4
+  );
 
-  std::cout << transpose(m1) << "\n";
+  Matrix2 m3 = submatrix(m1,0,0);
+  float deterimant = det(m3);
 
-  auto start = std::chrono::high_resolution_clock::now();
+  std::cout << m3 << "\n";
+  std::cout << deterimant << "\n";
+  std::cout << cof(m1) << "\n";
+  std::cout << inverse(m1) << "\n";
 
-  for (int i = 0; i < 1000000; i++) {
-    transpose(m1);
-  }
+  // Matrix3 m2(
+  //   2,0,0,
+  //   0,2,0,
+  //   0,0,2
+  // );
 
-  auto end = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> elapsed = end - start;
-  std::cout << "Elapsed time: " << elapsed.count() << " seconds" << std::endl;
+  // Matrix3 m3 = inverse(m1);
+
+  // std::cout << m1 << "\n";
+  // std::cout << m3 << "\n";
+  // std::cout << m3*m1 << "\n";
+
+  // auto start = std::chrono::high_resolution_clock::now();
+
+  // for (int i = 0; i < 1000000; i++) {
+  //   transpose(m1);
+  // }
+
+  // auto end = std::chrono::high_resolution_clock::now();
+  // std::chrono::duration<double> elapsed = end - start;
+  // std::cout << "Elapsed time: " << elapsed.count() << " seconds" << std::endl;
 }
