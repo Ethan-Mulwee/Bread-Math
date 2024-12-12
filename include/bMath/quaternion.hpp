6058abc214202b936f53de5ce1f79030e7bb1ca4
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2024-12-12 15:02:51 -0500

    Quaternion type

---
+++ include/bMath/quaternion.hpp
@@ -0,0 +1,25 @@
+#ifndef BMATH_QUATERNION
+#define BMATH_QUATERNION
+
+#include "vector.hpp"
+
+namespace bMath {
+  template<typename T>
+  struct Quaternion {
+    Vector<T,4> data;
+
+    Quaternion(T w, T x, T y, T z) {
+      data.x = x; data.y = y; data.z = z; data.w = w;
+    }
+
+    Quaternion(T angle, Vector<T,3> axis) {
+      data.w = cos(angle/2);
+      auto s = sin(angle/2);
+      data.x = s*axis.x;
+      data.y = s*axis.y;
+      data.z = s*axis.z;
+    }
+  };
+}
+
+#endif
